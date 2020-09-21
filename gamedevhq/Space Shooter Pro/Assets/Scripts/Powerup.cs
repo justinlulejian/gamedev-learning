47bd6776890f68dcefb9f78399fc3e44acf94b60
Justin Lulejian <jnlulejian@gmail.com>
2020-09-20 23:06:16 -0700

    Initial commit of 2D shooter and C# survival guide course progress

---
+++ gamedevhq/Space Shooter Pro/Assets/Scripts/Powerup.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+  [SerializeField]
+  private float _speed = 3.5f;
+
+  // ID for powerups
+  // 0 == triple
+  // 1 == speed
+  // 2 == shields
+  [SerializeField]
+  private int _powerupID;
+    
+  void Update()
+  {
+    transform.Translate(Vector3.down * _speed * Time.deltaTime);
+    if (transform.position.y <= -4.5f)
+    {
+      Destroy(this.gameObject);
+    }
+  }
+
+    private void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.tag == "Player")
+    {
+      Player player = other.transform.GetComponent<Player>();
+      if (player != null)
+      {
+        switch (_powerupID)
+        {
+          case 0:
+            player.TripleShotActive();
+            Debug.Log("Collected triple shot powerup");
+            break;
+          case 1:
+            player.SpeedBoostPowerupActive();
+            Debug.Log("Collected speed powerup");
+            break;
+          case 2:
+            player.ShieldsPowerupActive();
+            Debug.Log("Collected shield powerup");
+            break;
+          default:
+            Debug.Log("Collected a powerup with an unknown ID:" + _powerupID);
+            break;
+        }
+      }
+      Destroy(this.gameObject);
+    }
+  }
+}
