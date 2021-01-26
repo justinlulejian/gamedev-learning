eab885410098873dd97bfc543c502944f0200257
Justin Lulejian <justin@lulejian.com>
2021-01-26 01:28:33 -0800

    in progress balanced spawning feature, completed passing difficult choice to WaveManager, now to start of design/impl

---
+++ gamedevhq/Space Shooter Pro/Assets/Scripts/WaveManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    void Start()
+    {
+        string difficultySelection =  MainMenu.difficultySelection;
+        Debug.Log($"difficulty selection by user is: {difficultySelection}");
+    }
+
+    void Update()
+    {
+        
+    }
+}
