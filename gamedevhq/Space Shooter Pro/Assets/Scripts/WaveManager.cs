b77d19b58031b96d73589d86989e3ff95d1db0b2
Justin Lulejian <justin@lulejian.com>
2021-01-26 21:23:04 -0800

    balanced spawn interrupt

--- gamedevhq/Space Shooter Pro/Assets/Scripts/WaveManager.cs
+++ gamedevhq/Space Shooter Pro/Assets/Scripts/WaveManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,8 +7,15 @@ public class WaveManager : MonoBehaviour
 {
     void Start()
     {
-        string difficultySelection =  MainMenu.difficultySelection;
+        string difficultySelection = String.Empty;
+        difficultySelection =  MainMenu.difficultySelection != null? MainMenu.difficultySelection : String.Empty;
         Debug.Log($"difficulty selection by user is: {difficultySelection}");
+
+        if (string.IsNullOrEmpty(difficultySelection))
+        {
+            Debug.Log("Difficulty selection was not specified, defaulting to easy.");
+            difficultySelection = "easy";
+        }
     }
 
     void Update()
